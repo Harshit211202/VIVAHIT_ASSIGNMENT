730246cae4ba932a5b6d21ef00a81ae8cedf9520
Harshit211202 <guptah20@iitk.ac.in>
2023-04-08 02:29:06 +0530

    Added search functionality and also platform search, added a float action button for the company and changed some UI

--- assign_app/lib/game_list_screen.dart
+++ assign_app/lib/game_list_screen.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: library_private_types_in_public_api
+
 import 'package:flutter/material.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 
@@ -8,17 +10,32 @@ class GameListScreen extends StatefulWidget {
   const GameListScreen({super.key});
 
   @override
-  // ignore: library_private_types_in_public_api
   _GameListScreenState createState() => _GameListScreenState();
 }
 
 class _GameListScreenState extends State<GameListScreen> {
+  final TextEditingController _searchController = TextEditingController();
+  String? _platformFilter;
   List<Game> _games = [];
+
+  List<Game> _filteredGames = [];
   bool _isLoading = false;
+
+  get floatingActionButton => null;
+
   @override
- void initState() {
+  void initState() {
     super.initState();
     _fetchGames();
+    _searchController.addListener(() {
+      _onSearchTextChanged(_searchController.text);
+    });
+  }
+
+  @override
+  void dispose() {
+    _searchController.dispose();
+    super.dispose();
   }
 
   Future<void> _fetchGames() async {
@@ -26,9 +43,10 @@ class _GameListScreenState extends State<GameListScreen> {
       _isLoading = true;
     });
     try {
-      final games = await GameService.getGames(null);
+      final games = await GameService.getGames(_platformFilter);
       setState(() {
         _games = games;
+        _filteredGames = _games;
       });
     } finally {
       setState(() {
@@ -37,16 +55,70 @@ class _GameListScreenState extends State<GameListScreen> {
     }
   }
 
+  void _onPlatformFilterChanged(String? value) {
+    setState(() {
+      _platformFilter = value;
+    });
+    _fetchGames();
+  }
+
+  Future<void> _onSearchTextChanged(String value) async {
+    setState(() {
+      _games = _filteredGames
+          .where(
+              (game) => game.title.toLowerCase().contains(value.toLowerCase()))
+          .toList();
+    });
+
+    if (value.isEmpty) {
+      final games = await GameService.getGames(_platformFilter);
+      setState(() {
+        _games = games;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const   Text('GAMING WORLD', style: TextStyle(color: Color.fromARGB(255, 211, 11, 11)) ),
-        
-        backgroundColor: const Color.fromARGB(255, 32, 221, 228),
+        centerTitle: true,
+        title: const Text('GAMEZO',
+            style: TextStyle(color: Color.fromARGB(255, 255, 255, 255))),
+       
+        backgroundColor: const Color.fromARGB(255, 0, 4, 5),
       ),
       body: Column(
         children: [
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextField(
+              controller: _searchController,
+              onChanged: _onSearchTextChanged,
+              decoration: const InputDecoration(
+                hintText: 'Search by Title',
+                prefixIcon: Icon(Icons.search),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: DropdownButtonFormField<String>(
+              decoration: const InputDecoration(
+                labelText: 'Platforms',
+                labelStyle: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              value: _platformFilter,
+              items: const [
+                DropdownMenuItem(value: null, child: Text('All')),
+                DropdownMenuItem(value: 'browser', child: Text('Web Browser')),
+                DropdownMenuItem(value: 'pc', child: Text('PC')),
+              ],
+              onChanged: _onPlatformFilterChanged,
+            ),
+          ),
           _isLoading
               ? const Expanded(
                   child: Center(
@@ -60,19 +132,32 @@ class _GameListScreenState extends State<GameListScreen> {
                       final game = _games[index];
                       return ListTile(
                         leading: CachedNetworkImage(
+                        
                           imageUrl: game.thumbnailUrl,
                           width: 60,
                           height: 60,
-                          placeholder: (context, url) => const CircularProgressIndicator(),
-                          errorWidget: (context, url, error) => const Icon(Icons.error),
+                          placeholder: (context, url) =>
+                              const CircularProgressIndicator(),
+                          errorWidget: (context, url, error) =>
+                              const Icon(Icons.error),
                         ),
                         title: Text(game.title),
-                        subtitle: Text('${game.genre}    ${game.platform}   ${game.release_date}'),
+                        subtitle: Text(
+                            '${game.genre}    ${game.platform}   ${game.release_date}'),
                       );
                     },
                   ),
                 ),
         ],
+      ),
+      floatingActionButton: const FloatingActionButton(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(9.0)),
+        ),
+        onPressed: null,
+        backgroundColor: Color.fromARGB(255, 0, 0, 0),
+        elevation: 20.0,
+        child: Text('Vivahit', style: TextStyle(color: Color.fromARGB(255, 255, 254, 254)),)
       ),
     );
   }
--- assign_app/lib/main.dart
+++ assign_app/lib/main.dart
@@ -11,7 +11,7 @@ class GameListApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      title: 'Game List App',
+      // title: 'Game List App',
       home: GameListScreen(),
     );
   }
